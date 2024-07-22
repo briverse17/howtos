1435d953de407f0885711f79f344cfaa3fc30d5d
cnvs_minh_vu <nguyenminhvu@canvas-works.asia>
2024-07-22 11:11:49 +0700

    responsive: top, md

--- src/components/atoms/Top/Top.tsx
+++ src/components/atoms/Top/Top.tsx
@@ -1,6 +1,6 @@
 export function Top() {
     return (
-        <header className="bg-slate-800 top-0 flex items-center justify-center h-10 text-3xl text-white font-mono font-bold">
+        <header className="bg-slate-800 top-0 flex items-center justify-center md:h-10 md:text-3xl text-white font-mono font-bold">
             <span>How to...</span>
         </header>
     )
