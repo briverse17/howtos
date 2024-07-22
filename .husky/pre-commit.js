fa3d8df14bb8bdc608030cae8024255330ef4463
cnvs_minh_vu <nguyenminhvu@canvas-works.asia>
2024-07-22 10:49:29 +0700

    update pre-commit hook

--- .husky/pre-commit.js
+++ .husky/pre-commit.js
@@ -1,28 +1,43 @@
 const fs = require('fs')
 const path = require('path')
 
-const directoryPath = '.'
-const jsonFilePath = './contents.json'
+const directory = '.'
+const contentsFile = './contents.json'
 
-// Read existing JSON file or create an empty array if it doesn't exist
-const files = Object()
-if (fs.existsSync(jsonFilePath)) {
-    Object.assign(files, JSON.parse(fs.readFileSync(jsonFilePath, 'utf-8')))
+function updateContents(directory, contentFile) {
+    // Read existing JSON file or create an empty array if it doesn't exist
+    const files = Object()
+    if (fs.existsSync(contentFile)) {
+        Object.assign(files, JSON.parse(fs.readFileSync(contentFile, 'utf-8')))
+    }
+
+    // Read the directory and get all file names
+    const newFiles = Object()
+    fs.readdirSync(directory).map((file) => {
+        if (file.endsWith(".md") && !Object.hasOwn(files, file)) {
+            content = fs.readFileSync(path.join(directory, file), 'utf-8').split('\n').shift()
+            newFiles[file] = content
+        }
+    })
+
+    // // Merge existing file list with new file list
+    const updatedFiles = { ...files, ...newFiles }
+
+    // Write the updated file list back to JSON file
+    fs.writeFileSync(contentFile, JSON.stringify(updatedFiles, null, 2))
+
+    console.log(`Updated ${contentFile} with ${Object.keys(updatedFiles).length} files from ${directory}`)
 }
 
-// Read the directory and get all file names
-const newFiles = Object()
-fs.readdirSync(directoryPath).map((file) => {
-    if (file.endsWith(".md") && !Object.hasOwn(files, file)) {
-        content = fs.readFileSync(path.join(directoryPath, file), 'utf-8').split('\n').shift()
-        newFiles[file] = content
-    }
-})
+try {
+    // Scan the directory and update files.json
+    updateContents(directory, contentsFile)
 
-// // Merge existing file list with new file list
-const updatedFiles = { ...files, ...newFiles }
+    // Stage the updated files.json
+    execSync(`git add ${contentsFile}`)
 
-// Write the updated file list back to JSON file
-fs.writeFileSync(jsonFilePath, JSON.stringify(updatedFiles, null, 2))
-
-console.log(`Updated ${jsonFilePath} with ${Object.keys(updatedFiles).length} files from ${directoryPath}`)
+    // console.log('Pre-commit hook completed successfully.');
+} catch (error) {
+    console.error('Error in pre-commit hook:', error)
+    process.exit(1)
+}
