20bff616e9b4fcad7660caf9532391abfa3b65da
cnvs_minh_vu <nguyenminhvu@canvas-works.asia>
2024-07-22 11:22:31 +0700

    responsive: top & bot, md

--- src/components/atoms/Bottom/Bottom.tsx
+++ src/components/atoms/Bottom/Bottom.tsx
@@ -2,14 +2,14 @@ import logo from "../../../logo.svg"
 
 export function Bottom() {
     return (
-        <footer className="bg-slate-400 fixed inset-x-0 bottom-0 flex items-center justify-center h-10">
+        <footer className="bg-slate-400 fixed inset-x-0 bottom-0 flex items-center justify-center md:h-10">
             <a
                 href="https://github.com/briverse17/howtos/tree/content"
                 target="_blank"
                 rel="noopener noreferrer"
             ><img
                     src={logo}
-                    className="h-8 my-2 pointer-events-auto motion-safe:animate-logoSpin"
+                    className="md:h-8 md:my-2 h-5 my-1 pointer-events-auto motion-safe:animate-logoSpin"
                     alt="logo"
                 />
             </a>
--- src/components/atoms/Top/Top.tsx
+++ src/components/atoms/Top/Top.tsx
@@ -1,7 +1,7 @@
 export function Top() {
     return (
         <header className="bg-slate-800 top-0 flex items-center justify-center md:h-10 md:text-3xl text-white font-mono font-bold">
-            <span>How to...</span>
+            <span className="md:h-8 md:my-2 max-sm:leading-5 h-5 my-1">How to...</span>
         </header>
     )
 }
