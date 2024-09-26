ad722930555686684a2a5dcc04e7628d2eef1fe4
dd <vumnguyenn@gmail.com>
2024-09-26 21:15:52 +0700

    update config and actions

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,10 @@
 const nextConfig = {
     output: "export",
     basePath: "/howtos",
+    assetPrefix: "/howtos/",
+    trailingSlash: true,
     images: {
+        unoptimized: true,
         remotePatterns: [
             {
                 protocol: "https",
